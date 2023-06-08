e7c9db5a888870ed31cd6e99a1891e9173ca9174
Sephite <dallawebdev@gmail.com>
2023-06-08 13:14:24 +0200

    Décomposition du template en partials

---
+++ app/views/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <?php include_once '../app/views/template/partials/_head.php'; ?>
+  </head>
+  <body class="bg-gray-900 text-white font-sans leading-normal tracking-normal">
+    <?php include_once '../app/views/template/partials/_header.php';?>
+    <?php include_once '../app/views/template/partials/_main.php'; ?>
+    <?php include_once '../app/views/template/partials/_footer.php';?>
+  </body>
+</html>
---
+++ app/views/partials/_main.php
@@ -0,0 +1,6 @@
+<div class="container mx-auto flex flex-wrap pt-4 pb-12 text-white">
+      <?php include_once '../app/views/template/partials/_aside.php'; ?>
+
+      <!-- Main content -->
+      <?php echo $content ; ?>
+    </div>
